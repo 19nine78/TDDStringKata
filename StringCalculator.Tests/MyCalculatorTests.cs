b70c273794029e938bc68f2b065a004f8adc547d
19nine78 <19nine78@gmail.com>
2012-02-08 22:36:04 +0000

    First Commit :)

---
+++ StringCalculator.Tests/MyCalculatorTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class MyCalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            MyCalculator c = GetNewCalc();
+
+            int result = c.Add("");
+
+            Assert.AreEqual(0,result);
+        }
+
+        [Test]
+        public void Add_OneNumber_ReturnsThatNumber()
+        {
+            MyCalculator c = GetNewCalc();
+
+            int result = c.Add("1");
+
+            Assert.AreEqual(1,result);
+        }
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsTheSum()
+        {
+            MyCalculator c = GetNewCalc();
+
+            int result = c.Add("1,2");
+
+            Assert.AreEqual(3,result);
+        }
+
+        [TestCase("1,2,3",6)]
+        [TestCase("1,2,3,4", 10)]
+        public void Add_MultipleNumbers_ReturnsTheirSum(string numbers, int expected)
+        {
+            MyCalculator c = GetNewCalc();
+
+            int result = c.Add(numbers);
+
+            Assert.AreEqual(expected,result);
+        }
+
+
+        private static MyCalculator GetNewCalc()
+        {
+            return new MyCalculator();
+        }
+    }
+}
---
+++ StringCalculator/MyCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class MyCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+            {
+                return 0;
+            }
+
+            if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+
+            var nums = numbers.Split(',');
+
+            int total = 0;
+
+            foreach (var num in nums)
+            {
+                total = total + int.Parse(num);
+            }
+
+            return total;
+        }
+
+
+    }
+}
